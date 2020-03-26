6c119090e0e8bc7156deed6197bc9bc2815db376
Siva <44206012+r-siva@users.noreply.github.com>
2020-03-26 21:57:44 +0530

    Initial commit

---
+++ precache-manifest.a8b14ecc45207fffb5dfc69846372088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31390ef07f02ed3e5c87e65b867d42d",
+    "url": "/pick-your-emoji/index.html"
+  },
+  {
+    "revision": "0373129e98cf92a79f6e",
+    "url": "/pick-your-emoji/static/css/main.4346e7df.chunk.css"
+  },
+  {
+    "revision": "1341755da5723e2518f3",
+    "url": "/pick-your-emoji/static/js/2.03b97ce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pick-your-emoji/static/js/2.03b97ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0373129e98cf92a79f6e",
+    "url": "/pick-your-emoji/static/js/main.39f42861.chunk.js"
+  },
+  {
+    "revision": "5fed76d9fe26d9bc48fa",
+    "url": "/pick-your-emoji/static/js/runtime-main.7a03ed18.js"
+  }
+]);
